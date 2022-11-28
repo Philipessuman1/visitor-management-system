4f2d75dfd5524876b0617833016f783c8faba5a7
Philipessuman1 <philip.essuman@amalitech.org>
2022-11-28 19:56:35 +0000

    Qr code generator completed

--- frontend/src/components/QrcodeGenerator.tsx
+++ frontend/src/components/QrcodeGenerator.tsx
@@ -1,48 +1,128 @@
-import React, { useState } from 'react'
-import QRCode from 'react-qr-code'
-
-
+import React, {useState} from 'react'
+import QRCode from 'qrcode'
+import { useForm } from 'react-hook-form'
 
 
 const QrcodeGenerator = () => {
+    const [url, setUrl] = useState('')
+    const [qrcode, setQrcode] = useState('')
 
-     
-        const [url, setUrl] = useState('')
+    const GenerateQRCode = () => {
+        QRCode.toDataURL(url, (err, url) => {
+            if (err) return console.error(err)
 
-        const downloadQRCode = (e: React.FormEvent) => {
-            e.preventDefault()
+            console.log(url)
+            setQrcode(url)
+        })
+    }
 
 
-            setUrl('')
-        }
-
-        const qrCode = (
-            <QRCode 
-                id='23'
-                size={500}
-                value={'https://google.com'}
-                bgColor='white'
-                fgColor='black'
-                level=''
-            />
-        )
-
+    const {
+      control,
+      register,
+      handleSubmit,
+      formState: { errors }
+    } = useForm();
+  
+    const onSubmit = (data:{}) => {
+      setUrl(JSON.stringify(data))
+      
+    };
 
   return (
-    <div>
-        <form onSubmit={downloadQRCode}>
-            <input 
+    <div className='d-flex justify-content-between mt-4'>
+      <form className='container w-50 form-bgColor ' onSubmit={handleSubmit(onSubmit)}>
+            <h3>Schedule Appointment with Visitor Details</h3>
+            <div className="mb-3">
+              <label className='form-label'>Name</label>
+              <input 
+                className='form-control'
+                placeholder='eg.Daryl Jones'
                 type='text'
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder='https://example.com'
-            />
+                {...register('name', {
+                  required:true,
+                  minLength: 2
+                })}
+              />
+              {errors.name && errors.name.type === "required" && (
+                <p className="errorMessage">Name is required.</p>
+              )}
+              {errors.name && errors.name.type === "minLength" && (
+                <p className="errorMessage">
+                  Name should be valid
+                </p>
+              )}
+              </div>
+              <div className="mb-3">
+              <label className='form-label'>Email</label>
+              <input
+                className='form-control'
+                placeholder='eg. daryljones@gmail.com'
+                type="text"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+                })}
+              />
+              {errors.email && errors.email.type === "required" && (
+                <p className="errorMessage">Email is required.</p>
+              )}
+              {errors.email && errors.email.type === "pattern" && (
+                <p className="errorMessage">Email is not valid.</p>
+              )}
+            </div>
+            <div className="mb-3">
+              <label className='form-label'>Company Name</label>
+              <input
+                className='form-control'
+                placeholder='eg.Microsoft Ghana' 
+                type='text'
+                {...register('company', {
+                  required:true,
+                  minLength: 2
+                })}
+              />
+              {errors.company && errors.company.type === "required" && (
+                <p className="errorMessage">Company Name is required.</p>
+              )}
+              {errors.company && errors.company.type === "minLength" && (
+                <p className="errorMessage">
+                  Company name should be valid
+                </p>
+              )}
+              </div>
+            <div className="mb-3">
+              <label className='form-label'>Phone Number</label>
+              <input
+                className='form-control'
+                placeholder='eg.0548012053'
+                type="phone"
+                {...register("phone", {
+                  required: true,
+                  minLength: 10
+                })}
+              />
+              {errors.phone && errors.phone.type === "required" && (
+                <p className="errorMessage">Phone number is required.</p>
+              )}
+              {errors.phone && errors.phone.type === "minLength" && (
+                <p className="errorMessage">
+                  Phone number is incomplete
+                </p>
+              )}
+            </div>
+            <button className='btn btn-success m-auto' type="submit">Submit</button>
+            </form>
+            <div className='w-50'>
+                <h3>Click Generate to generate QrCode for your guest</h3>
+                <button onClick={GenerateQRCode}>Generate</button>
+                {qrcode && <>
+                    <img src={qrcode}   />
+                    <a href={qrcode} download='qrcode.png'>Download</a>
+                </>}
+            </div>
+        
 
-            <button type='submit'>Download QR Code</button>
-        </form>
-
-        <div>{qrCode}</div>
-      
     </div>
   )
 }
--- frontend/src/pages/Host.tsx
+++ frontend/src/pages/Host.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
-
+import { NavLink, useNavigate } from 'react-router-dom';
 import QrcodeGenerator from "../components/QrcodeGenerator";
+import profilepic from '../assets/amalitech_logo2.jpg'
+import { useForm } from 'react-hook-form'
 
 
 
 export default function Host(){
+
+  const navigate = useNavigate();
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data:{}) => {
+    console.log(data);
+    return data
+    navigate('/')
+  };
+
+
   return (
     <div>
-      host module
+      <div className='d-flex justify-content-between m-0 mb-0 nav-background w-100'>
+        <NavLink to='/' 
+        style={{color:'white', textDecoration: 'none',marginLeft:'2rem', marginTop:'1rem'}}>
+          Home
+        </NavLink>
+        <div className='d-flex align-items-center justify-content-center mb-0 mt-1 mx-2'>
+          <img className='profile-pic' src={profilepic} alt='profile-pic' />
+          <p className='username'>Username</p>
+        </div>
+      </div>
+
+      <div className='d-flex justify-content-between appointment-form'>
+        <div>
+          <QrcodeGenerator />
+        </div>
+      </div>
     </div>
   )
 }
