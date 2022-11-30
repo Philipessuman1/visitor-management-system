b82b8ee66c22b0568d4314c1abfc9c4c70409855
emmanuel-odotei <emmanuel.odotei@amalitech.org>
2022-11-30 01:58:39 -0800

    modified db file

--- server/db.ts
+++ server/db.ts
@@ -1,27 +1,26 @@
 import {Pool} from 'pg';
 import 'dotenv/config';
 
-/*const devConfig = {
+const devConfig = {
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT as unknown as number
 };
-*/
 
-/*const proConfig = {
+const proConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
       }
-};*/
+};
 
-export const pool = new Pool( {
+/*export const pool = new Pool( {
     user: "postgres",
     password: "postgres",
     database: "postgres",
     host: "localhost",
     port: 5432
-});
-//export const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
+});*/
+export const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
